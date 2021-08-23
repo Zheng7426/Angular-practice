d92bfe015629f3073700685b1adca0729086e7a8
Hz <36901033+Zheng7426@users.noreply.github.com>
2021-08-23 10:13:45 +0800

    shipping component with cart service

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -24,8 +24,7 @@ export class CartService {
     return this.items;
   }
 
-  getShippingPrice() {
-    return this.http.get<{type: string, price: number}>
-    ('/assets/shipping.json');
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
 }
